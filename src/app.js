64309d4118a89d7b4360055d11d05a799a87fba1
ramesh <ramesh0533@gmail.com>
2017-04-16 21:57:42 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+import Game from './components/Game';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Game starts in few hours 1 2 3..</h1>
+      </div>
+    )
+  }
+}
+
+export default Game;
